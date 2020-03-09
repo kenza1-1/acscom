cd6ce4554dbe21d19ff8a588d9ad4b1125a9ed10
iovlyclick <haceux@gmail.com>
2020-03-09 13:13:20 +0000

    panier v1

---
+++ src/Controller/CartController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Cart\CartService;
+
+class CartController extends AbstractController {
+
+    /**
+     * @Route("/cart", name="cart_index")
+     */
+    public function index(CartService $cartService) {
+
+        return $this->render('cart/index.html.twig', [
+            'items' => $cartService->getFullCart(), 
+            'total' => $cartService->getTotal()
+        ]);
+
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     */
+    public function add($id, CartService $cartService){
+
+        $cartService->add($id);
+
+        return $this->redirectToRoute("cart_index");
+
+    }
+
+    /**
+     * @Route("/cart/remove/{id}", name="cart_remove")
+     */
+    public function remove($id, CartService $cartService){
+       
+        $cartService->remove($id);
+
+        return $this->redirectToRoute("cart_index");
+    }
+}
